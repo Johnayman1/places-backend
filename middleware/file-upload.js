2fbeda3a6be516fbdc0b68177fbff548c6793806
Johnayman1 <johnayman03@gmail.com>
2026-02-20 21:39:42 +0200

    second commit

--- middleware/file-upload.js
+++ middleware/file-upload.js
@@ -1,27 +1,49 @@
-const multer = require("multer");
-const { v4: uuid } = require("uuid");
+// const multer = require("multer");
+// const { v4: uuid } = require("uuid");
 
-const MIME_TYPE_MAP = {
-  "image/png": "png",
-  "image/jpg": "jpg",
-  "image/jpeg": "jpeg",
-};
-const fileUpload = multer({
-  limits: 500000,
-  storage: multer.diskStorage({
-    destination: (req, file, cb) => {
-      cb(null, "uploads/images");
-    },
-    filename: (req, file, cb) => {
-      const ext = MIME_TYPE_MAP[file.mimetype];
-      cb(null, uuid() + "." + ext);
-    },
-  }),
-  fileFilter: (req, file, cb) => {
-    const isValid = !!MIME_TYPE_MAP[file.mimetype];
-    let error = isValid ? null : new Error("Invalid mime type.");
-    cb(error,isValid);
+// const MIME_TYPE_MAP = {
+//   "image/png": "png",
+//   "image/jpg": "jpg",
+//   "image/jpeg": "jpeg",
+// };
+// const fileUpload = multer({
+//   limits: 500000,
+//   storage: multer.diskStorage({
+//     destination: (req, file, cb) => {
+//       cb(null, "uploads/images");
+//     },
+//     filename: (req, file, cb) => {
+//       const ext = MIME_TYPE_MAP[file.mimetype];
+//       cb(null, uuid() + "." + ext);
+//     },
+//   }),
+//   fileFilter: (req, file, cb) => {
+//     const isValid = !!MIME_TYPE_MAP[file.mimetype];
+//     let error = isValid ? null : new Error("Invalid mime type.");
+//     cb(error,isValid);
+//   },
+// });
+
+// module.exports = fileUpload;
+
+const multer = require("multer");
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+const cloudinary = require("cloudinary").v2;
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_KEY,
+  api_secret: process.env.CLOUD_SECRET,
+});
+
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: "mern-places", // folder in Cloudinary
+    allowed_formats: ["jpg", "png", "jpeg"],
   },
 });
 
-module.exports = fileUpload;
+const upload = multer({ storage: storage });
+
+module.exports = upload;
