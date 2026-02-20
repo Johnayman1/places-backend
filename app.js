098f1e52578cc67c9b9f8930fb32176fb04b5ba3
Johnayman1 <johnayman03@gmail.com>
2026-02-20 23:01:31 +0200

    second commit

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "*",
+    origin: "https://places-frontend-git-main-johnayman1s-projects.vercel.app/",
     methods: ["GET", "POST", "PATCH", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   }),
